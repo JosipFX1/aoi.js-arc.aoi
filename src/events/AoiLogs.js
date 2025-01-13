9c0da6751164133f9cdc163bbe7744a4104f1ba5
JosipFX <josip9510@gmail.com>
2025-01-13 11:03:23 +0100

    Edited

--- src/events/AoiLogs.js
+++ src/events/AoiLogs.js
@@ -1,1 +1,25 @@
+const AoiError = require("../classes/AoiError.js");
 
+module.exports = async () => {
+    try {
+        AoiError.createConsoleMessage(
+            [
+                {
+                    text: `Installed on V1.0.2`,
+                    textColor: "green"
+                },
+                {
+                    text: "https://discord.gg/MmJ3PQWVSS",
+                    textColor: "blue"
+                }
+            ],
+            "white",
+            {
+                text: "arc.aoi",
+                textColor: "cyan"
+            }
+        );
+    } catch (err) {
+        console.error(`ArcLogs: Error during initialization: ${err}`);
+    }
+};
--- src/events/AoiStart.js
+++ src/events/AoiStart.js
@@ -1,7 +1,7 @@
 const Interpreter = require("../core/interpreter.js");
 
 module.exports = async (client) => {
-    if (client.aoiOptions.aoiLogs !== false) {
+    if (client.aoiOptions.aoiLogs === true) {
         await require("./AoiLogs.js")(client);
     }
 
